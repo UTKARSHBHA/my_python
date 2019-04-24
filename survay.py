45aa5404ed1923fde9312023ad34420f59b42362
UTKARSHBHA <ub2092000@gmial.com>
2019-04-24 15:16:30 +0530

    Create survay.py

---
+++ survay.py
@@ -0,0 +1,9 @@
+
+while(True):
+    n = int(input("enter input:\n"))
+    if n>100:
+        print("congratulations!you have intered a number greater than hundred")
+        break
+    else:
+        print("try again!\n")
+        continue
